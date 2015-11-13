667ab258aa03df00434dec1fb097114e559b1a20
satbeka <satbeka@gmail.com>
2015-11-13 16:15:01 +0600

    131115

--- src/dboperation/loginInfo/UserLoginInfo.java
+++ src/dboperation/loginInfo/UserLoginInfo.java
@@ -1,9 +1,47 @@
 package dboperation.loginInfo;
 
 
-public class UserLoginInfo {
+import java.io.Serializable;
 
+public class UserLoginInfo implements Serializable {
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public boolean isConnect() {
+        return isConnect;
+    }
+
+    public void setIsConnect(boolean isConnect) {
+        this.isConnect = isConnect;
+    }
+
+    private String login;
+    private String pwd;
+    private boolean isConnect;
+    private String session;
+    private static final long serialVersionUID = 1L;
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
 }
 
 
